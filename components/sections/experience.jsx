e155b8b8840db2ac3c07a6e83e783d405f780c8b
Sush3871 <sushanrshirke96.com>
2025-11-09 01:00:35 +0530

    bg logo made smaller

--- components/sections/experience.jsx
+++ components/sections/experience.jsx
@@ -26,7 +26,7 @@ const ExperienceCard = ({ experienceData, className = "" }) => {
 
   return (
     <div className={`w-full relative z-10 group ${className}`}>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[30%] aspect-square bg-theme-primary rounded-full blur-lg -z-10"></div>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[16%] aspect-square bg-theme-primary rounded-full blur-lg -z-10"></div>
       <div className="w-full h-full dark:bg-[#ffffff9a] bg-theme-secondary dark:bg-opacity-70 bg-opacity-70 flex flex-col items-start justify-between gap-2 sm:gap-4 md:gap-6 lg:gap-8 p-4 md:p-6 lg:p-8 rounded-[30px] md:rounded-[40px] z-20 backdrop-blur-lg dark:hover:bg-opacity-100 hover:bg-opacity-100 ease-out duration-300 transition-all group">
         <div className="w-full flex flex-wrap items-center gap-2 lg:gap-6 justify-between">
           <div className="flex items-center gap-2 lg:gap-4 pr-7 rounded-full bg-secondary dark:bg-secondary">
